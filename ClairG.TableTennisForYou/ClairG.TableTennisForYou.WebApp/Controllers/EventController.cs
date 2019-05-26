3ed4a6159b1d91d482197c449b24c09dc5a48183
lemon <lemon>
2019-05-25 23:47:16 -0400

    1.[ADD] EventController
    2.[ADD] add link of events

---
+++ ClairG.TableTennisForYou/ClairG.TableTennisForYou.WebApp/Controllers/EventController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ClairG.TableTennisForYou.WebApp.Controllers
+{
+    public class EventController : Controller
+    {
+        //Event/Monthly Event
+        public ActionResult MEvent()
+        {
+            return View();
+        }
+        //Event/ITTF World Tour
+        public ActionResult WTour()
+        {
+            return View();
+        }
+        //Event/World Champions
+        public ActionResult WChampions()
+        {
+            return View();
+        }
+        //Event/Tokyo 2020
+        public ActionResult Tokyo()
+        {
+            return View();
+        }
+    }
+}
